757671fab71f61df3b780bd0f9eccc906f5b5ee2
Biel <vgabriel@id.uff.br>
2020-12-30 21:26:19 -0300

    adicionados dataset para verificar se os objetos são iguais;

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,48 @@
 const cards = document.querySelectorAll(".card");
+let hasFippedCard = false;
+let fistCard, secondCard;
 
 function flipCard()
 {
-    this.classList.toggle("flip");
+    this.classList.add("flip");
+
+    if(!hasFippedCard)
+    {
+        hasFippedCard = true;
+        fistCard = this;
+        return;
+    }
+    else
+    {
+        secondCard = this;
+    }
+    hasFippedCard = false;
+    checkForMath();
 }
 
 cards.forEach((card) => {
     card.addEventListener('click', flipCard);
 });
+
+function checkForMath()
+{
+    if(fistCard.dataset.card === secondCard.dataset.card)
+    {
+        disableCards();
+        return;
+    }
+    else
+    {
+        unflipCards();
+    }
+}
+
+function disableCards()
+{
+
+}
+
+function unflipCards()
+{
+
+}
